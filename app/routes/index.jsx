4410b0e4a8f5ad5cfc579d80bb6f1348a7e14110
Adrian <adrberia@gmail.com>
2021-12-16 20:09:38 -0400

    first commit

---
+++ app/routes/index.jsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <p>
+        Remix is a full stack web framework by the creators of React Router.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
